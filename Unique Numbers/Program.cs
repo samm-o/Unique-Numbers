bb0f8dd22becadd60d159a565828969e5f1b25d7
samm-o <samuilsgeorgiev@gmail.com>
2023-01-30 09:50:02 -0500

    Add project files.

---
+++ Unique Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Unique_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter 5 unique integers seperated by a hyphen: ");
+                var input = Console.ReadLine();
+                if (input == "")
+                {
+                    Console.WriteLine("Please Retry.");
+                    break;
+                }
+
+                var numbers = Array.ConvertAll(input.Split('-'), int.Parse).ToList();
+                if (numbers.Count != numbers.Distinct().Count())
+                {
+                    Console.WriteLine("Duplicate.");
+                    break;
+                }
+                else if (numbers.Count == numbers.Distinct().Count())
+                {
+                    Console.WriteLine("Not duplicate.");
+                    break;
+                }
+            }
+            
+        }
+    }
+}
